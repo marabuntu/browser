aa5d90ad0c7b6026b45c413a12847373bcde3b60
Edgar Borovik <partus@tut.by>
2023-05-23 12:18:16 +0800

    Initial

---
+++ browser/browser/browserApp.swift
@@ -0,0 +1,17 @@
+//
+//  browserApp.swift
+//  browser
+//
+//  Created by Edgar Borovik on 23/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct browserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
